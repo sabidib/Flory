33362fc1e447375686815b0440dc5395e275efde
Samy Abidib <Abidibs@gmail.com>
2014-08-14 18:52:38 -0300

    Added Box2D.js

---
+++ src/objects/box2D.js
@@ -0,0 +1,30 @@
+/**
+ * @author sabidib
+ */
+
+
+Flory.Box2D = function(x,y,width,height,name){
+	Flory.Entity.call(this);
+	this.name = (name == undefined ? "Box2D" : name);
+	this.x = (x == undefined ? 0 : x);
+	this.y = (y == undefined ? 0 : y);
+	this.width = (width == undefined ? 0 : width);
+	this.height = (height == undefined ? 0 :height);
+}
+
+Flory.Box2D.prototype = Object.create(Flory.Entity);
+
+Flory.Box2D.prototype.contructor = Flory.Box2D;
+
+
+
+
+
+
+
+
+
+
+
+
+
