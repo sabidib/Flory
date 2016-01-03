b5c7bd985fdd05d08d16a0c234d1b72e3124c12d
Samy Abidib <abidibs@gmail.com>
2016-01-03 12:47:44 -0500

    Updated Lennard jones and newtonian, as well as added continuousField to the source order

--- build/demo/lennardJones/lennardJones.js
+++ build/demo/lennardJones/lennardJones.js
@@ -14782,6 +14782,43 @@ Flory.baseField.prototype.scale = function(num){
  * @author sabidib http://github.com/sabidib
  */
 
+/** @constructor */
+Flory.ContinuousField = function(field_function){
+	Flory.baseField.call(this,[]);
+	this.scaler = 1;
+	this.field_function = (field_function !== undefined) ? field_function : function(){};
+}
+
+
+Flory.ContinuousField.prototype = Object.create(Flory.baseField.prototype);
+
+
+Flory.ContinuousField.prototype.constructor = Flory.ContinuousField;
+
+Flory.ContinuousField.prototype.getForce = function(position){
+	return this.field_function(position).mult(this.scaler);
+}
+
+Flory.ContinuousField.prototype.scale =function(num){
+	if(typeof num === "number"){
+		this.scaler = num;		
+	}
+	return this;
+}
+
+Flory.ContinuousField.prototype.clone = function(){
+	return new Flory.ContinuousField(this.field_function);
+}
+
+
+
+
+
+
+/**
+ * @author sabidib http://github.com/sabidib
+ */
+
 /**
  * Creates a field where a vector is associated with an approximate position.
  * @constructor
