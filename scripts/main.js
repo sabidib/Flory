43c83f6c9c696fad22fed90a46d33b0d3f61a940
Samy Abidib <fox@navier.local>
2016-01-28 18:42:06 -0500

    Updated site

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,4 @@
-var requirejs = require('requirejs');
-
-requirejs.config({
-	shim : {
-        "bootstrap" : { "deps" :['jquery'] }
-    },
-    paths: {
-        "jquery" : "//code.jquery.com/jquery-2.1.1.min",
-        "bootstrap" :  "//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min"  
-    },
-    nodeRequire: require
-});
-
-
-requirejs(['jquery', 'bootstrap', 'prettify'], function($){
+requirejs(['jquery', 'bootstrap','scripts/prettify.js','scripts/anchor.js'], function($){
 
     // DOM ready
     $(function(){
@@ -20,6 +6,3 @@ requirejs(['jquery', 'bootstrap', 'prettify'], function($){
         // Twitter Bootstrap 3 carousel plugin
     });
 });
-
-
-
